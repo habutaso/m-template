78d20dc3fcb37eb619cafeabef8df316636b1ad3
habutaso <habutaso@gmail.com>
2024-02-12 21:54:23 +0900

    fix: storybook

--- front-end/.storybook/main.ts
+++ front-end/.storybook/main.ts
@@ -1,16 +1,22 @@
 import type { StorybookConfig } from "@storybook/nextjs";
+import path from "path";
 
 const config: StorybookConfig = {
-  stories: [
-    "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
-  ],
+  stories: ["../**/*.mdx", "../**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
+    "@storybook/addon-themes"
   ],
+  webpackFinal: async (baseConfig) => {
+    baseConfig.resolve.alias = {
+      ...baseConfig.resolve.alias,
+      "@": path.resolve(__dirname, "../"),
+    };
+    return baseConfig;
+  },
   framework: {
     name: "@storybook/nextjs",
     options: {},
